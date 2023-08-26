a8538f7cfb2bcd6c4920db8f420d111ab0057141
Al-Hassan <hkamel904@gmail.com>
2023-08-26 13:36:43 +0300

    Trying to solve the quiz with the initial version but it didn't work and now I'm modifying them

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from Answers import *
 
 
-input_file_path = r"E:\My Study\Courses\Coursera Projects\Python for Genomics Data Science\dna.example.fasta"
+input_file_path = r"E:\My Study\Courses\Coursera Projects\Python for Genomics Data Science\dna2.fasta"
 
 output_file_one = "out_1.txt"
 output_file_two = "out_2.txt"
 output_file_three = "out_3.txt"
 
 Answer_Q1_2(input_file_path, output_file_one)
-Answer_Q3(input_file_path, output_file_two)
-Answer_Q4(input_file_path, output_file_three, 4)
+#Answer_Q3(input_file_path, output_file_two)
+Answer_Q4(input_file_path, output_file_three, 6)
